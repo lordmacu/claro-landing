d4cda7cdd7e65b07fb97bbd11bb361c747437b4b
Cristian Garcia <cristiangarcia@MacBook-Pro-de-Cristian.local>
2020-01-08 20:50:49 -0500

    s

--- src/components/App.js
+++ src/components/App.js
@@ -30,6 +30,7 @@ class App extends  React.Component {
                     </div>
                     
                     
+                    
                     <Route path="/">
                         <div className="section-secundaria">
                             <SectionListCenter ref="child" />
